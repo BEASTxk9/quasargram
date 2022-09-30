9f36e94516b0f5b9571edf941d4bca0f8f4c891b
BEASTxk9 <shanestevensxk9@gmail.com>
2022-09-30 16:13:47 +0200

    update

---
+++ Desktop/VUE/quasar-projects/quasargram/src/layouts/MainLayout.vue
@@ -0,0 +1,76 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <!-- header -->
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="constrain">
+
+        <!-- camera btn -->
+        <q-btn to="/camera" class="large-screen-only q-mr-sm" flat round icon="eva-camera-outline" size="18px" dense />
+
+        <q-separator
+        class="large-screen-only"
+        vertical
+        spaced
+         />
+
+        <!-- logo -->
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Quasargram
+        </q-toolbar-title>
+
+        <!-- home btn -->
+        <q-btn to="/" class="large-screen-only" flat round icon="eva-home-outline" size="18px" dense />
+
+      </q-toolbar>
+
+
+
+    </q-header>
+
+    <!-- main -->
+    <q-page-container class="bg-grey-1">
+      <router-view />
+    </q-page-container>
+
+    <!-- footer -->
+    <q-footer class="small-screen-only bg-white text-grey-10" bordered>
+
+      <q-tabs active-color="primary" indicator-color="transparent">
+        <q-route-tab to="/" icon="eva-home-outline" />
+        <q-route-tab to="/camera" icon="eva-camera-outline" />
+      </q-tabs>
+
+    </q-footer>
+
+
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  data() {
+    return {
+
+    }
+  },
+})
+</script>
+
+<style lang="sass">
+.q-toolbar
+ @media (min-width: $breakpoint-sm-min)
+  height: 60px
+
+.q-toolbar__title
+ font-size: 30px
+ @media (max-width: $breakpoint-xs-max)
+  text-align: center
+
+.q-footer
+ .q-tab__icon
+  font-size: 30px
+</style>
---
+++ Desktop/VUE/quasar-projects/quasargram/src/pages/PageHome.vue
@@ -0,0 +1,139 @@
+<template>
+  <q-page class="constrain q-pa-md">
+
+<div class="row q-col-gutter-lg">
+
+  <!-- left posts -->
+  <div class="col-12 col-sm-8">
+
+ <q-card
+    v-for="post in posts"
+    :key="post.id"
+    class="card-post q-mb-md" flat bordered>
+         <!-- user data header -->
+      <q-item>
+
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+
+          <q-item-label class="text-bold">
+            BEASTxk9
+          </q-item-label>
+
+          <q-item-label caption>
+            {{ post.location }}
+          </q-item-label>
+
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+<!-- user post data -->
+      <q-img
+        :src="post.imageUrl"
+      />
+
+      <q-card-section>
+        <div>{{ post.caption }}</div>
+        <div class="text-caption text-grey">
+          {{ niceDate(post.date) }}
+        </div>
+      </q-card-section>
+
+    </q-card>
+
+  </div>
+
+  <!-- right profile -->
+  <div class="col-4 large-screen-only">
+
+    <q-item class="fixed">
+
+<q-item-section avatar>
+  <q-avatar size="48px">
+    <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+  </q-avatar>
+</q-item-section>
+
+<q-item-section>
+
+  <q-item-label class="text-bold">
+    BEASTxk9
+  </q-item-label>
+
+  <q-item-label caption>
+    Shane Stevens
+  </q-item-label>
+
+</q-item-section>
+</q-item>
+
+  </div>
+
+</div>
+
+
+
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { date } from 'quasar';
+
+export default defineComponent({
+  name: 'PageHome',
+  data(){
+    return {
+      posts: [
+        {
+          id: '1',
+          caption: 'Golden Gate Bridge',
+          date: 1664521178835,
+          location: 'Cape Town, Western Cape, South Africa',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id: '2',
+          caption: 'Golden Gate Bridge',
+          date: 1664521178835,
+          location: 'Cape Town, Western Cape, South Africa',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id: '3',
+          caption: 'Golden Gate Bridge',
+          date: 1664521178835,
+          location: 'Cape Town, Western Cape, South Africa',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id: '4',
+          caption: 'Golden Gate Bridge',
+          date: 1664521178835,
+          location: 'Cape Town, Western Cape, South Africa',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+      ]
+    }
+  },
+  methods: {
+    niceDate(value){
+return date.formatDate(value, 'MMMM D h:mmA')
+    }
+  }
+})
+</script>
+
+<style lang="sass">
+.card-post
+ .q-img
+  min-height: 200px
+</style>
